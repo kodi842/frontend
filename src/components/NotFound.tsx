e707a9bd95ab6523945d6f267409dcde3f40fb67
kodi842 <maksimorisenko9@gmail.com>
2025-07-06 16:10:07 +0300

    Add files via upload

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return (
+    <div style={{ padding: "4rem", textAlign: "center" }}>
+      <h1 style={{ fontSize: "48px", color: "#00ffc1" }}>404</h1>
+      <p style={{ fontSize: "24px", color: "#fff" }}>Страница не найдена</p>
+    </div>
+  );
+};
+
+export default NotFound;
